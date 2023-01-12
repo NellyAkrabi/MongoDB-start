f734d871a4444dbf36b0e5d4de3c52ec3c362f05
Nelly Akrabi <103043065+NellyAkrabi@users.noreply.github.com>
2023-01-12 14:56:41 +0100

    Add files via upload

---
+++ src/model.ts
@@ -0,0 +1,22 @@
+export const getApiInstructions = () => {
+	return `
+<style>
+	body {
+		background-color: #444;
+		padding: 1rem;
+		color: #fff;
+		font-family: courier;
+	}
+	code {
+		background-color: #333;
+	}
+	a {
+		color: yellow;
+	}
+</style>
+<h1>Book Site API</h1>
+<ul>
+	<li><a href="employees">/employees</a> - get all employees</li>
+</ul>
+	`;
+}
---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import * as model from './model.js';
+import express from 'express';
+import cors from 'cors';
+import * as config from './config.js';
+
+const app = express();
+app.use(cors());
+
+app.get('/', (req: express.Request, res: express.Response) => {
+	res.send(model.getApiInstructions());
+});
+
+app.listen(config.port, () => {
+	console.log(`listening on port http://localhost:${config.port}`);
+});
